b88a9c8eeee3a1caaf04b8e887dfc4c80eba6ca0
Brandon LaDuke <BrandonLaDuke@users.noreply.github.com>
2020-04-25 00:44:41 -0400

    Fixed typo in site title

--- header.php
+++ header.php
@@ -6,7 +6,7 @@ session_start(); ?>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Spineless Bound | Sullivan University Book CLub</title>
+    <title>Spineless Bound | Sullivan University Book Club</title>
     <link rel="stylesheet" href="css/master.css">
   </head>
   <body>
