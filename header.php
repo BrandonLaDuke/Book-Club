ae0f20b62ea976f62f2e9dcbf2f6fcb618914da2
Brandon LaDuke <brandon@brandonladuke.com>
2020-05-01 09:26:35 -0400

    Enable ga

--- header.php
+++ header.php
@@ -16,14 +16,14 @@ session_start(); ?>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
 
-    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-66276915-6"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-66276915-6"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
       gtag('config', 'UA-66276915-6');
-    </script> -->
+    </script>
 
   </head>
   <body>
