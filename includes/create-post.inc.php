e2468bede1c836a0d4bcf91992e29f6374594c91
Brandon LaDuke <brandon@brandonladuke.com>
2020-08-30 11:59:00 -0400

    Removed bookworm from sending a message in Discord everytime a new post is posted.

--- includes/create-post.inc.php
+++ includes/create-post.inc.php
@@ -37,24 +37,11 @@ if (isset($_POST['post'])) {
     ORDER BY postId DESC;";
     $result = mysqli_query($conn, $sql);
     $resultCheck = mysqli_num_rows($result);
-      if ($resultCheck > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-          $postId = $row['postId'];
-        }
+    if ($resultCheck > 0) {
+      while ($row = mysqli_fetch_assoc($result)) {
+        $postId = $row['postId'];
       }
-
-    $webhookurl = $bookworm_webhook;
-    $msg = $username . " posted in SpinelessBound!";
-    $json_data = array ('content'=>"$msg", "username" => "Bookworm");
-    $make_json = json_encode($json_data);
-    $ch = curl_init( $webhookurl );
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-    curl_setopt( $ch, CURLOPT_POST, 1);
-    curl_setopt( $ch, CURLOPT_POSTFIELDS, $make_json);
-    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt( $ch, CURLOPT_HEADER, 0);
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-    $response = curl_exec( $ch );
+    }
 
     header("Location: ../index.php?success=post");
     exit();
--- settings.php
+++ settings.php
@@ -20,10 +20,38 @@
 <?php if (isset($_SESSION['userId'])) { ?>
   <div class="settings-container">
     <h1>Settings</h1>
+    <h2>Notification Prefrences</h2>
     <div class="setting">
       <span class="setting__title">Push Notifications</span>
       <button onclick="subscribe()" class="setting__button profile-btn btn lined thin" type="button" name="button">Enable Push Notifications</button>
-      <p class="setting__description">Enabling push notifications will allow us to send you notifications whenever someone Likes or comments on your post, rates your book, or when there is an important announcement.</p>
+      <p class="setting__description">Enabling push notifications will allow us to send you notifications to your device whenever someone Likes or comments on your post, rates your book, or when there is an important announcement.</p>
+    </div>
+    <div class="setting">
+      <span class="setting__title">Email Notifications</span>
+      <button onclick="subscribe()" class="setting__button profile-btn btn lined thin" type="button" name="button">Enable Email Notifications</button>
+      <p class="setting__description">Enabling Email notifications will allow us to send you notifications via email whenever someone Likes or comments on your post, rates your book, or when there is an important announcement.</p>
+    </div>
+    <h2>Customize your notifications</h2>
+    <div class="setting">
+      <span class="setting__title">Get notified for all posts in the stream</span>
+      <button onclick="subscribe()" class="setting__button profile-btn btn lined thin" type="button" name="button">Turn on</button>
+      <p class="setting__description">Enabling this will aleart you when there is a new post in the stream.</p>
+    </div>
+    <div class="setting">
+      <span class="setting__title">Spineless Bound Email List</span>
+      <button onclick="subscribe()" class="setting__button profile-btn btn lined thin" type="button" name="button">Turn off</button>
+      <p class="setting__description">Turn this off if you wish too no longer recieve email communications from Spinelessbound.</p>
+    </div>
+    <h2>My Account</h2>
+    <div class="setting">
+      <span class="setting__title">Suspend your Membership</span>
+      <button onclick="subscribe()" class="setting__button profile-btn btn lined thin" type="button" name="button">I want to suspend my membership</button>
+      <p class="setting__description">Suspending your membership will deactivate your account and suspend your membership to the Spineless Bound Book Club. Your account will remain intact. If you would like to rejoin the club contact the the Spineless Bound Student Leader.</p>
+    </div>
+    <div class="setting">
+      <span class="setting__title">Delete your Account</span>
+      <button onclick="subscribe()" class="setting__button profile-btn btn lined thin" type="button" name="button">I want to Delete my Account</button>
+      <p class="setting__description">Deleting your account will delete all user data associated with your account, including your posts, comments, and ratings. This action in not reverseable.</p>
     </div>
   </div>
 
