df2c5ba78e72b5298bc2923d521bb802cad13f0a
Brandon LaDuke <brandon@brandonladuke.com>
2020-08-31 05:08:22 -0400

    fixed

--- includes/timeElapsed.inc.php
+++ includes/timeElapsed.inc.php
@@ -1,11 +1,20 @@
 <?php
 function timeElapsed($date){
-        $months=array();
+
         date_default_timezone_set("America/Los_Angeles");
-        for ($i=1; $i < 13; $i++) {
-            $month = date('F',mktime(0,0,0,$i));
-            $months += [substr($month,0,3) => $i];
-        }
+        $months = array(
+          "Jan"=>"01",
+          "Feb"=>"02",
+          "Mar"=>"03",
+          "Apr"=>"04",
+          "May"=>"05",
+          "Jun"=>"06",
+          "Jul"=>"07",
+          "Aug"=>"08",
+          "Sep"=>"09",
+          "Oct"=>"10",
+          "Nov"=>"11",
+          "Dec"=>"12");
         $date_year = date('Y', strtotime($date));//year of the date
         $date_month = date('m', strtotime($date));//month of the date
         $date_day = date('d', strtotime($date));//day of the date
